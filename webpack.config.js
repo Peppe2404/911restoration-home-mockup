63cdc49f686a09b1b13385e63d890230552dd307
Peppe <141502459+Peppe2404@users.noreply.github.com>
2024-07-29 18:47:49 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,18 +5,11 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
     publicPath: './'
   },
   module: {
     rules: [
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/[hash][ext][query]'
-        }
-      },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
@@ -28,16 +21,43 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name].[hash][ext]'
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name].[hash][ext]'
+        }
+      }
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+      filename: 'index.html'
     }),
   ],
   devServer: {
     static: path.join(__dirname, 'build'),
     compress: true,
     port: 3000,
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  performance: {
+    hints: false,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
   },
 };
