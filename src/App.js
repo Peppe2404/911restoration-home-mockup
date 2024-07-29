dbf3d5c7b14b006410125067e94a307aa3700215
Peppe <141502459+Peppe2404@users.noreply.github.com>
2024-07-29 18:46:33 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const logo = require('./assets/911_restoration_logo_transparent.png');
+const logo = import('./assets/911_restoration_logo_transparent.png');
 
 const TextSlideshow = ({ texts }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
